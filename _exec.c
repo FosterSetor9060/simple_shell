00ab994284517ccc12df4168b9bddf0d600c2a8a
Foster Setor <walkerdziwornu@gmail.com>
2023-12-14 02:13:53 -0800

    _exec.c

--- _exec.c
+++ _exec.c
@@ -40,7 +40,8 @@ void _exec(char **args, char *echo)
 				execve("/bin/echo", args, environ);
 			}
 		}
-		else if (strcmp(args[0], "ls") == 0 && strchr(getenv("PATH"), '/') != NULL)
+		else if (strcmp(args[0], "ls") == 0 && strchr(getenv("PATH"), '/') != NULL
+				&& getenv("PATH") != NULL)
 		{
 			execve("/bin/ls", args, environ);
 		}
